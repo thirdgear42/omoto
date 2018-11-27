bfc10993976a2d3dbc6c0691aedc719d88427a43
Sudhir <sudhir@liftoffllc.com>
2018-11-27 11:01:41 +0530

    survey form added

--- script.js
+++ script.js
@@ -98,17 +98,3 @@ function decreaseLength(çurrentLength) {
   document.getElementById("stepNum").innerHTML = currentStep;
   stepArr[currentStep]();
 }
-
-// function move() {
-//   var elem = document.getElementById("myBar");
-//   var width = 1;
-//   var id = setInterval(frame, 10);
-//   function frame() {
-//     if (width >= 100) {
-//       clearInterval(id);
-//     } else {
-//       width++;
-//       elem.style.width = width + "%";
-//     }
-//   }
-// }
